de1e683b9fe81b69d46b7c681608fee9acac9625
Vincent Pun <pun.vincent@gmail.com>
2018-06-02 22:41:23 +0800

    Housekeep: Add production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,11 @@
+const baseConfig = require('./webpack.config');
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DotenvPlugin = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -42,17 +47,44 @@ module.exports = {
           {
             loader: 'awesome-typescript-loader',
             options: {
-              configFileName: path.join(__dirname, 'tsconfig.dev.json'),
+              configFileName: path.join(__dirname, 'tsconfig.json'),
             }
           },
         ],
       },
-      { test: /\.s?css$/, use: ['style-loader','css-loader', 'sass-loader'] },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: (loader) => [
+                require('autoprefixer')(),
+              ],
+            }
+          },
+          'sass-loader',
+        ],
+      },
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
     ]
   },
 
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': 'production',
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html',
@@ -63,5 +95,11 @@ module.exports = {
     new DotenvPlugin({
       safe: true,
     }),
-  ]
+  ],
+
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin(),
+    ]
+  },
 }
